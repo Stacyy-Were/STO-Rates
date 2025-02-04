59c030c3a60f302ca6f681148da0a42c5461415a
Stacy Were <95570406+Stacyy-Were@users.noreply.github.com>
2025-02-04 10:52:22 +0300

    connected to phpmyAdmin

---
+++ serverconnect.php
@@ -0,0 +1,16 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mailchimp";  // Replace with your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
